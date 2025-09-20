4d85ec49f148d066a1220fd4417ad256098b21ad
kanggui <kg.he@outlook.com>
2025-09-20 10:27:20 +0800

    feat: support refresh scanned devices

--- src/main.rs
+++ src/main.rs
@@ -54,49 +54,53 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 /// Scan HRS devices, and return a device which user selected
+// TODO: This implementation is very stupid, improve it.
 async fn select_device(adapter: &Adapter) -> Result<bluest::Device, bluest::Error> {
     use std::time::{Duration, Instant};
 
     let mut devices = Vec::new();
     let mut devices_stream = adapter.discover_devices(&[HRS_UUID]).await?;
     println!("Scanning HRS devices...");
-    let start = Instant::now();
-    while devices.is_empty() || Instant::now().duration_since(start) < Duration::from_millis(1500) {
-        if let Some(Ok(device)) = devices_stream.next().await
-            && !devices.contains(&device)
-        {
-            devices.push(device);
-        }
-    }
-    println!("Done");
-
-    // Print devices
-    for (i, device) in devices.iter().enumerate() {
-        println!("{}) {device}", i + 1);
-    }
-
-    // Let user select a device
-    let mut input = String::new();
     loop {
-        print!("\nInput a device index (q: quit, 0: refresh): ");
-        std::io::stdout().flush().unwrap();
-
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read from stdin.");
-
-        if let Ok(index) = input.trim().parse::<usize>()
-            && index > 0
-            && index <= devices.len()
+        let start = Instant::now();
+        while devices.is_empty()
+            || Instant::now().duration_since(start) < Duration::from_millis(500)
         {
-            let device = devices.into_iter().nth(index - 1).unwrap();
-            println!("Selected Device: [{}] {}", device, device.id());
-            return Ok(device);
-        } else if input.trim() == "q" {
-            std::process::exit(0);
-        } else if input.trim() == "0" {
-            unimplemented!("Unimplement refresh");
+            if let Some(Ok(device)) = devices_stream.next().await
+                && !devices.contains(&device)
+            {
+                devices.push(device);
+            }
         }
-        input.clear();
+
+        // Print devices
+        for (i, device) in devices.iter().enumerate() {
+            println!("{}) {device}", i + 1);
+        }
+
+        // Let user select a device
+        let mut input = String::new();
+        loop {
+            print!("\nInput a device index (q: quit, 0: refresh): ");
+            std::io::stdout().flush().unwrap();
+
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read from stdin.");
+
+            if let Ok(index) = input.trim().parse::<usize>()
+                && index > 0
+                && index <= devices.len()
+            {
+                let device = devices.into_iter().nth(index - 1).unwrap();
+                println!("Selected Device: [{}] {}", device, device.id());
+                return Ok(device);
+            } else if input.trim() == "q" {
+                std::process::exit(0);
+            } else if input.trim() == "0" {
+                break;
+            }
+            input.clear();
+        }
     }
 }
